a02b7cc8740e6c41ba64766a86c2a50a80d315e3
CodeWithHaiderr <mazahirhaider98@gmail.com>
2024-12-13 22:41:39 +0500

    final app complete

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:4001",
+        origin: "https://chatappmern-7jft.onrender.com",
         methods: ["GET", "POST"]
     }
 });
